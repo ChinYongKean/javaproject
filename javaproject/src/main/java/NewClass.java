331f2d8a7df8950f36eab7afca3485b9e0ffb00c
yc <>
2021-11-05 23:17:06 +0800

    plssss

--- javaproject/src/main/java/NewClass.java
+++ javaproject/src/main/java/NewClass.java
@@ -16,7 +16,7 @@ public class NewClass {
         System.out.print("Hello YongKean");
         System.out.print("Hello YongKean");
         System.out.print("Hello YongKean");
-        System.out.print("Hello YongKean");
+        
 }
 }
     
